0dce715e306e60ec7b9104564265d56dd37321b8
gubataso <gutierrezrendell06@gmail.com>
2023-09-26 00:46:16 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import './App.css'
 import { toast } from 'react-toastify'
 
 function App() {
-  const [TodoToggle, setTodoToggle] = useState(null)
-  const [newTitle, setNewTitle] = useState("")
-  const [newDescription, setNewDescription] = useState("")
-  const [Todo, setTodo] = useState([])
-
+  const [todoToggle, setTodoToggle] = useState(null)
+  const [todos, setTodos] = useState(() => {
+    return JSON.parse(localStorage.getItem("Todos")) ?? []
+  })
+  const [todo, setTodo] = useState({
+    id:null,
+    title:"",
+    description:""
+  })
+  
   useEffect(() => {
-    console.log("test")
-  }, [Todo])
+    localStorage.setItem("Todos", JSON.stringify(todos))
+  }, [todos])
   
   const handleSumbmit = e =>{
     e.preventDefault()
-    if(newTitle.length === 0 || newDescription.length === 0){
+    if(todo.title.length === 0 || todo.description.length === 0){
       toast.error('All fields are required!', {
             position: "top-right",
             autoClose: 5000,
@@ -26,10 +31,10 @@ function App() {
             theme: "dark",
           });
     }else{
-      setTodo(currentTodo => {
+      setTodos(currentTodo => {
         return [
           ...currentTodo,
-          {id: crypto.randomUUID(), title: newTitle,description: newDescription, isCompleted:false},
+          {id: crypto.randomUUID(), title: todo.title,description: todo.description, isCompleted:false},
         ]
       })
       toast.success('Todo has been added!', {
@@ -42,13 +47,16 @@ function App() {
         progress: undefined,
         theme: "dark",
       });
-      setNewTitle("")
-      setNewDescription("")
+      setTodo({
+        id:null,
+        title:"",
+        description:""
+      })
     }
   }
     
   const handleDeleteClick = id => {
-    setTodo(currentTodos => {
+    setTodos(currentTodos => {
       return currentTodos.filter(todo => todo.id !== id)
     })
   }
@@ -56,55 +64,16 @@ function App() {
 
 
   const handleToggleCompleted = id => {
-    const todos = [...Todo]
+    const todovar = [...todos]
     todos.map(todo => {
       if(todo.id === id){
         todo.isCompleted = !todo.isCompleted
       }
       return todo
     })
-    setTodo(todos)
+    setTodos(todovar)
   }
-  // function handleSumbmit(e){
-  //   e.preventDefault()
-  //   if(newTitle.length === 0 || newDescription.length === 0){
-  //     toast.error('All fields are required!', {
-  //           position: "top-right",
-  //           autoClose: 5000,
-  //           hideProgressBar: false,
-  //           closeOnClick: true,
-  //           pauseOnHover: true,
-  //           draggable: true,
-  //           progress: undefined,
-  //           theme: "dark",
-  //         });
-  //   }else{
-  //     setTodo(currentTodo => {
-  //       return [
-  //         ...currentTodo,
-  //         {id: crypto.randomUUID(), title: newTitle,description: newDescription, isCompleted:false},
-  //       ]
-  //     })
-  //     toast.success('Todo has been added!', {
-  //       position: "top-right",
-  //       autoClose: 5000,
-  //       hideProgressBar: false,
-  //       closeOnClick: true,
-  //       pauseOnHover: true,
-  //       draggable: true,
-  //       progress: undefined,
-  //       theme: "dark",
-  //     });
-  //     setNewTitle("")
-  //     setNewDescription("")
-  //   }
-  // }
-
-  // function handleDeleteClick(id){
-  //   setTodo(currentTodos => {
-  //     return currentTodos.filter(todo => todo.id !== id)
-  //   })
-  // }
+  
 
   // function handleToggleCompleted(id){
   //   const todos = [...Todo]
@@ -114,9 +83,10 @@ function App() {
   //     }
   //     return todo
   //   })
-  //   setTodo(todos)
+  //   setTodos(todos)
   // }
-  // setTodo(currentTodo =>{
+  
+  // setTodos(currentTodo =>{
   //   return currentTodo.map(todo => {
   //     if(todo.id === id) {
   //         if(isCompleted == true){
@@ -134,16 +104,16 @@ function App() {
   return (
     <>
     <div className='text-center p-4'> 
-      <button className='rounded-2xl p-2 bg-gradient-to-l from-cyan-500 to-blue-500 text-white' onClick={() => setTodoToggle(TodoToggle=>!TodoToggle)}>
-        {TodoToggle ? "Hide" : "Show"} Form
+      <button className='rounded-2xl p-2 bg-gradient-to-l from-cyan-500 to-blue-500 text-white' onClick={() => setTodoToggle(todoToggle=>!todoToggle)}>
+        {todoToggle ? "Hide" : "Show"} Form
       </button>
-      {TodoToggle ?
+      {todoToggle ?
         <div>
           <div className='p-2'>
-            <input placeholder='Title' value={newTitle} onChange={e => setNewTitle(e.target.value)} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 focus:outline-none' />
+            <input placeholder='Title' value={todo.title} onChange={e => setTodo({...todo, title: e.target.value})} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 focus:outline-none' />
           </div>
           <div className='p-2'>
-            <input placeholder='Description' value={newDescription} onChange={e => setNewDescription(e.target.value)} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 focus:outline-none' />
+            <input placeholder='Description' value={todo.description} onChange={e => setTodo({...todo, description:e.target.value})} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 focus:outline-none' />
           </div>
           <div>
             <button onClick={handleSumbmit}>
@@ -155,11 +125,11 @@ function App() {
       }
       
     </div>
-    {!TodoToggle ? 
+    {!todoToggle ? 
       <div className=''>
         <h3 className='text-center mt-5 pb-10'>Todo List</h3>
         <div className='xl:grid xl:grid-cols-5 lg:grid lg:grid-cols-3 lg:place-items-center flex gap-2 flex-wrap justify-center'>
-          {Todo.map(todo =>{
+          {todos.map(todo =>{
             return(
               <div key={todo.id} className='container border xl:my-3 xl:p-4 xl:w-80 pb-2 rounded-md shadow-md '>
                 <div  className='text-center'>
